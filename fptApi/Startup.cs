384f339e7e8623574438b8ade213296be2efe27b
4lexRossi <devalexrossi@gmail.com>
2020-10-22 20:39:53 -0300

    api-new

--- fptApi/Startup.cs
+++ fptApi/Startup.cs
@@ -11,7 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace fpt_Api
+namespace fptApi
 {
     public class Startup
     {
